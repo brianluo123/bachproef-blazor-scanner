7c15ee27bf7c8427c661de93adc7e76c4a626f83
Zhao Luo <zhao.luo@student.hogent.be>
2024-04-06 12:25:05 +0200

    Add scan form

--- BachelorproefBlazorScanner.Domain/Scans/Scan.cs
+++ BachelorproefBlazorScanner.Domain/Scans/Scan.cs
@@ -5,11 +5,4 @@ public class Scan
     public string Barcode { get; set; }
     public string Zone { get; set; }
     public string Destination { get; set; }
-
-    public Scan(string barcode, string zone, string destination)
-    {
-        Barcode = barcode;
-        Zone = zone;
-        Destination = destination;
-    }
 }
